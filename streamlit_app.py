20e63c9c6b57f3b4421f4b4e10a7820005e90e54
rrSnowflake <124082983+rrSnowflake@users.noreply.github.com>
2023-02-01 12:59:12 -0500

    Update streamlit_app.py
    removed extra line...

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,3 @@ streamlit.dataframe(my_data_rows)
 # allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
-streamlit.write('The user entered ', add_my_fruit)
